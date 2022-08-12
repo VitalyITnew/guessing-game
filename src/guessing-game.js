f68c86cf5cd6e03a1a60162fe80f964d06b0fe03
VitalyItnew <103492966+VitalyITnew@users.noreply.github.com>
2022-08-12 20:09:36 +0300

    feat:add js file

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.minRange;
+        this.maxRange;
+        this.num;
+    }
+        
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
     }
 
     guess() {
-
+        this.num = Math.ceil((this.maxRange - this.minRange) / 2 + this.minRange);
+        return this.num;
     }
 
     lower() {
-
+        this.maxRange = this.num;
     }
 
     greater() {
-
+        this.minRange = this.num;
     }
 }
 
